46600ef0550cf84819d12bd3ff4cf5019aa05bf7
Tabitha Maru <65254062+Tby23rd@users.noreply.github.com>
2021-03-10 20:30:03 -0800

    Add files via upload
    There is a separate folder for Netbeans as it is the app used to build PHP projects

---
+++ display_results.php
@@ -0,0 +1,104 @@
+<?php
+
+    // get the data from the form
+    $investment = filter_input(INPUT_POST, 'investment',
+        FILTER_VALIDATE_FLOAT);
+    $interest_rate = filter_input(INPUT_POST, 'interest_rate',
+        FILTER_VALIDATE_FLOAT);
+    $years = filter_input(INPUT_POST, 'years',
+        FILTER_VALIDATE_INT);
+    $compoundInterest = filter_input(INPUT_POST, 'compoundInterest');
+    
+    // validate investment
+    if ($investment === FALSE ) {
+        $error_message = 'Investment must be a valid number.'; 
+    } else if ( $investment <= 0 ) {
+        $error_message = 'Investment must be greater than zero.'; 
+    // validate interest rate
+    } else if ( $interest_rate === FALSE )  {
+        $error_message = 'Interest rate must be a valid number.'; 
+    } else if ( $interest_rate <= 0) {
+        $error_message = 'Interest rate must be greater than zero .';
+    } else if ( $interest_rate > 15 ) {
+        $error_message = 'Interest rate must be less than or equal to 15.';
+    // validate years
+    } else if ( $years === FALSE ) {
+        $error_message = 'Years must be a valid whole number.';
+    } else if ( $years <= 0 ) {
+        $error_message = 'Years must be greater than zero.';
+    } else if ( $years > 30 ) {
+        $error_message = 'Years must be less than 31.';
+    // set error message to empty string if no invalid entries
+    }
+
+/*
+     else if ( $compoundInterest === FALSE ) {
+     $error_message = 'No';}
+          else if ( $compoundInterest === TRUE ) {
+     $error_message = 'Yes';}
+*/
+
+    // set error message to empty string if no invalid entries
+    else  {
+        $error_message = ''; }
+
+    // if an error message exists, go to the index page
+    if ($error_message != '') {
+        include('index.php');
+        exit();
+    }
+
+    // calculate the future value
+    $future_value = $investment;
+    if ($compoundInterest == 'on')  {
+        for ($i = 1; $i <= $years * 12; $i++) {
+            $future_value = ($future_value + ($future_value * $interest_rate *(.01/12)));
+        }
+    }
+    
+    else {
+    $future_value = $investment;
+        for ($i = 1; $i <= $years; $i++) {
+            $future_value = ($future_value + ($future_value * $interest_rate *.01));
+        }
+    }
+
+    // apply currency and percent formatting
+    $investment_f = '$'.number_format($investment, 2);
+    $yearly_rate_f = $interest_rate.'%';
+$future_value_f = '$'.number_format($future_value, 2);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Futurer</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+    <main>
+        <h1>Future </h1>
+
+        <label>Investment Amount:</label>
+        <span><?php echo htmlspecialchars($investment_f); ?></span><br />
+
+        <label>Yearly Interest Rate:</label>
+        <span><?php echo htmlspecialchars($yearly_rate_f); ?></span><br />
+
+        <label>Number of Years:</label>
+        <span><?php echo htmlspecialchars($years); ?></span><br />
+
+        <label>Future Value:</label>
+        <span><?php echo htmlspecialchars($future_value_f); ?></span><br />
+        
+        <label>Compound monthly:</label>
+        <span><?php if ($compoundInterest == 'on') {
+                    echo 'Yes';
+            } else {
+                    echo 'No';
+            }
+         ?></span><br />
+        
+        <p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
+    </main>
+</body>
+</html>
